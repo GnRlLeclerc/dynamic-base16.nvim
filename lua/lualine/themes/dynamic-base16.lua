17ba3c0fd6db41c363fecfca9c4a7419af40d96c
Thibaut de Saivre <thibaut2saivre@gmail.com>
2024-07-10 22:39:27 +0200

    Fix lualine theme

--- lua/lualine/themes/dynamic-base16.lua
+++ lua/lualine/themes/dynamic-base16.lua
@@ -6,7 +6,39 @@ local base16 = require('dynamic-base16')
 local colors = base16.colors
 
 -- Taken from the lualine base16 theme
-return {
+local function setup(colors)
+  local theme = {
+    normal = {
+      a = { fg = colors.bg, bg = colors.normal },
+      b = { fg = colors.light_fg, bg = colors.alt_bg },
+      c = { fg = colors.fg, bg = colors.bg },
+    },
+    replace = {
+      a = { fg = colors.bg, bg = colors.replace },
+      b = { fg = colors.light_fg, bg = colors.alt_bg },
+    },
+    insert = {
+      a = { fg = colors.bg, bg = colors.insert },
+      b = { fg = colors.light_fg, bg = colors.alt_bg },
+    },
+    visual = {
+      a = { fg = colors.bg, bg = colors.visual },
+      b = { fg = colors.light_fg, bg = colors.alt_bg },
+    },
+    inactive = {
+      a = { fg = colors.dark_fg, bg = colors.bg },
+      b = { fg = colors.dark_fg, bg = colors.bg },
+      c = { fg = colors.dark_fg, bg = colors.bg },
+    },
+  }
+
+  theme.command = theme.normal
+  theme.terminal = theme.insert
+
+  return theme
+end
+
+return setup({
   bg = colors.base01,
   alt_bg = colors.base02,
   dark_fg = colors.base03,
@@ -16,4 +48,4 @@ return {
   insert = colors.base0B,
   visual = colors.base0E,
   replace = colors.base09,
-}
+})
